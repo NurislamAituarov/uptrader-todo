92276708241837358ee5c9c171b0a9c8b47f31bb
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-10-01 20:32:03 +0600

    add reactive form subtask

--- src/components/table/change-task/ChangeTask.tsx
+++ src/components/table/change-task/ChangeTask.tsx
@@ -1,23 +1,55 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, MouseEvent, useEffect, useRef, useState } from 'react';
 import style from './ChangeTask.module.scss';
 import { useAppSelector } from '../../../hooks/redux';
 
 interface IProps {}
 
+interface ISubtask {
+  title: string;
+  id: string;
+}
+interface IForm {
+  id: number;
+  title: string;
+  description: string;
+  priority: string;
+  file: null;
+  subtasks: ISubtask[];
+}
+
 export function ChangeTask() {
   const item = useAppSelector((state) => state.state.taskItem);
-
-  const [form, setForm] = useState({
+  const [dropdownPriority, setDropdownPriority] = useState(false);
+  const [form, setForm] = useState<IForm>({
+    id: 0,
     title: '',
     description: '',
     priority: '',
     file: null,
+    subtasks: [],
   });
+
+  const refWrapper = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    document.body.addEventListener('click', (e: any) => {
+      const target = e.target as HTMLElement;
+      if (refWrapper.current && refWrapper.current.contains(target)) {
+        setDropdownPriority(false);
+      }
+    });
+  }, []);
 
   useEffect(() => {
     item &&
       setForm((state) => {
-        return { ...state, title: item.title, description: item.description };
+        return {
+          ...state,
+          id: item.id,
+          title: item.title,
+          description: item.description,
+          priority: item.priority ? item.priority : '',
+        };
       });
   }, [item]);
 
@@ -28,15 +60,108 @@ export function ChangeTask() {
     });
   }
 
+  function handleInputSubtaskChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value, id } = e.target;
+    const newSubtask = form.subtasks.map((el) => {
+      if (el.id === id) {
+        return { ...el, title: value };
+      }
+      return el;
+    });
+    setForm((data: any) => {
+      return { ...data, subtasks: newSubtask };
+    });
+  }
+
+  function selectPriorityList(value: string) {
+    setForm((state) => {
+      return {
+        ...state,
+        priority: value,
+      };
+    });
+
+    setDropdownPriority(false);
+  }
+
+  function onActivatePriority(e: MouseEvent<HTMLSpanElement>) {
+    e.stopPropagation();
+    setDropdownPriority((value) => !value);
+  }
+
+  function addNewSubTask() {
+    setForm((data) => {
+      data.subtasks.push({
+        title: '',
+        id: new Date().getTime() + '',
+      });
+      return { ...data };
+    });
+  }
+
   return (
-    <div className={style.wrapper}>
-      <input
-        className={style.title}
-        name="title"
-        type="text"
-        value={form.title}
-        onChange={handleInputChange}
-      />
+    <div ref={refWrapper} className={style.wrapper}>
+      <form>
+        <input
+          className={style.title}
+          name="title"
+          type="text"
+          value={form.title}
+          onChange={handleInputChange}
+        />
+        <div className={style['wrapper-input']}>
+          <label>
+            Приоритет задачи:
+            <span onClick={(e) => onActivatePriority(e)} className={style['priority-active']}>
+              {form.priority ? form.priority : '-'}
+            </span>
+          </label>
+          {dropdownPriority && (
+            <div className={style['dropdown-priority']}>
+              {['high', 'medium', 'low'].map((value, i) => {
+                return (
+                  <div
+                    key={i}
+                    onClick={() => selectPriorityList(value)}
+                    className={style['priority-list']}>
+                    {value}
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+
+        <div className={style['wrapper-input']}>
+          <label htmlFor="description">Описание</label>
+          <textarea
+            className={style['description-text']}
+            name="description"
+            id="description"
+            value={form.description}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className={style['wrapper-subtask']}>
+          <div onClick={addNewSubTask} className={style['subtask-add']}>
+            +<p>Добавить подзадачу</p>
+          </div>
+
+          {form.subtasks.map((task) => {
+            return (
+              <div key={task.id} className={style['subtask-item']}>
+                <input
+                  type="text"
+                  name="title"
+                  id={task.id}
+                  onChange={(e) => handleInputSubtaskChange(e)}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </form>
     </div>
   );
 }
